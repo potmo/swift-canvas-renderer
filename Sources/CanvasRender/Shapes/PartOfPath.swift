0f4026c7ada25a3540032307eae07ef6f4a833c8
Nisse Bergman <git@potmo.com>
2023-11-21 16:20:06 +0100

    make arrays of PartOfPath also a PartOfPath

--- Sources/CanvasRender/Shapes/PartOfPath.swift
+++ Sources/CanvasRender/Shapes/PartOfPath.swift
@@ -3,7 +3,14 @@ import Foundation
 import simd
 import SwiftUI
 
-
 public protocol PartOfPath {
     func drawPartOfPath(in context: RenderContext)
+}
+
+extension [PartOfPath]: PartOfPath {
+    public func drawPartOfPath(in context: RenderContext) {
+        for part in self {
+            part.drawPartOfPath(in: context)
+        }
+    }
 }

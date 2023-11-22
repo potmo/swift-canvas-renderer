ccac067bedffb3cbf2dbfe765b96b932f6e6d75f
Nisse Bergman <git@potmo.com>
2023-11-22 19:29:46 +0100

    make orbits have a resolution so longer arcs get more steps and smaller frewer

--- Sources/CanvasRender/Shapes/AxisOrbit.swift
+++ Sources/CanvasRender/Shapes/AxisOrbit.swift
@@ -9,13 +9,15 @@ public struct AxisOrbit: DrawableShape, PartOfPath {
     let angle: Double
     let radius: Double
     let axis: simd_double3
+    let arcResolutuon: Double
 
-    public init(pivot: simd_double3, point: simd_double3, angle: Double, axis: Vector) {
+    public init(pivot: simd_double3, point: simd_double3, angle: Double, axis: Vector, arcResolutuon: Double = 0.1) {
         self.pivot = pivot
         self.point = point
         self.angle = angle
         self.radius = (point - pivot).length
         self.axis = axis
+        self.arcResolutuon = arcResolutuon
     }
 
     public func draw(in context: RenderContext) {
@@ -28,10 +30,17 @@ public struct AxisOrbit: DrawableShape, PartOfPath {
         let lever = (point - pivot).normalized.scaled(by: radius)
         context.renderTarget.addLine(to: context.transform(point))
 
-        for t in stride(from: 0.0, through: 1.0, by: 0.01) {
-            let rot = simd_quatd(angle: angle * t, axis: axis)
+        let arcLength = angle * radius
+
+        for t in stride(from: 0.0, through: arcLength, by: arcResolutuon) {
+            let interpolatedAngle = angle * t / arcLength
+            let rot = simd_quatd(angle: interpolatedAngle, axis: axis)
             let drawPoint = pivot + rot.act(lever)
             context.renderTarget.addLine(to: context.transform(drawPoint))
         }
+
+        let rot = simd_quatd(angle: angle, axis: axis)
+        let drawPoint = pivot + rot.act(lever)
+        context.renderTarget.addLine(to: context.transform(drawPoint))
     }
 }
--- Sources/CanvasRender/Shapes/Orbit.swift
+++ Sources/CanvasRender/Shapes/Orbit.swift
@@ -8,20 +8,23 @@ public struct Orbit: DrawableShape, PartOfPath {
     let point: simd_double3
     let rotation: simd_quatd
     let spokes: Bool
+    let arcResolutuon: Double
 
-    public init(pivot: simd_double3, point: simd_double3, rotation: simd_quatd, spokes: Bool = false) {
+    public init(pivot: simd_double3, point: simd_double3, rotation: simd_quatd, spokes: Bool = false, arcResolutuon: Double = 0.1) {
         self.pivot = pivot
         self.point = point
         self.rotation = rotation
         self.spokes = spokes
+        self.arcResolutuon = arcResolutuon
     }
 
-    public init(center: simd_double3, radius: Double, startAngle: Double, endAngle: Double, planeNormal: Vector, spokes: Bool = false) {
+    public init(center: simd_double3, radius: Double, startAngle: Double, endAngle: Double, planeNormal: Vector, spokes: Bool = false, arcResolutuon: Double = 0.1) {
         self.pivot = center
         let lever = planeNormal.arbitraryOrthogonal
         self.point = Quat(angle: startAngle, axis: planeNormal).act(lever)
         self.rotation = Quat(angle: endAngle - startAngle, axis: planeNormal)
         self.spokes = spokes
+        self.arcResolutuon = arcResolutuon
     }
 
     public func draw(in context: RenderContext) {
@@ -30,7 +33,7 @@ public struct Orbit: DrawableShape, PartOfPath {
         context.renderTarget.strokePath()
 
         if spokes {
-            context.renderTarget.setStrokeColor(NSColor(context.color.opacity(0.1)).cgColor)
+            context.renderTarget.setStrokeColor(context.color.opacity(0.1).cgColor)
             let lever = point - pivot
             for t in stride(from: 0.0, through: 1.0, by: 0.1) {
                 let rot = simd_slerp(.identity, rotation, t)
@@ -40,7 +43,7 @@ public struct Orbit: DrawableShape, PartOfPath {
                 context.renderTarget.addLine(to: context.transform(drawPoint))
                 context.renderTarget.strokePath()
             }
-            context.renderTarget.setStrokeColor(NSColor(context.color).cgColor)
+            context.renderTarget.setStrokeColor(context.color.cgColor)
             
             Circle(center: pivot, radius: 1).draw(in: context)
             
@@ -53,10 +56,15 @@ public struct Orbit: DrawableShape, PartOfPath {
 
         context.renderTarget.move(to: context.transform(point))
 
-        for t in stride(from: 0.0, through: 1.0, by: 0.01) {
+        let arcLength = rotation.angle * lever.length
+
+        for arcDistance in stride(from: 0.0, through: arcLength, by: arcResolutuon) {
+            let t = arcDistance / arcLength
             let rot = simd_slerp(.identity, rotation, t)
             let drawPoint = (pivot + rot.act(lever))
             context.renderTarget.addLine(to: context.transform(drawPoint))
         }
+        let drawPoint = (pivot + rotation.act(lever))
+        context.renderTarget.addLine(to: context.transform(drawPoint))
     }
 }
